fde1a706934f3ef73eb3b28e8b9dcd8987988251
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2020-05-09 17:20:38 -0700

    more help for TIT2 tags

--- tagit.pl
+++ tagit.pl
@@ -23,6 +23,29 @@ for my $podcast (keys %db) {
   push @{$db{$podcast}}, @mp3s;
 }
 
+# save each mp3 file's title (TIT2)
+# to do: would it be useful to bundle everything into a single data structure?
+my %titles;
+{
+  use lib('MP3-Tag-1.15/lib');
+  use MP3::Tag;
+  # blacklisted podcasts with missing or misleading titles
+  my @blacklist = ("On the Media", "Planet Money");
+  for my $podcast (sort keys %db) {
+    my $dir = "$podcastbase/$podcast";
+    my $blacklisted = grep {/$podcast/} @blacklist;
+    for my $episode (sort @{$db{$podcast}}) {
+      $titles{$episode} = "";
+      if (!$blacklisted) {
+        my $mp3file = "$dir/$episode";
+        my $mp3 = MP3::Tag->new($mp3file);
+        $mp3->get_tags;
+        $titles{$episode} = $mp3->{ID3v2}->get_frame("TIT2") || "";
+      }
+    }
+  }
+}
+
 print ".PHONY: all\n";
 print "all: cp TALB TCON\n";
 print "\n";
@@ -55,9 +78,12 @@ print "\n";
 print "# set title (TIT2) - not auto-run, to be manually modified\n";
 print ".PHONY: TIT2\n";
 print "TIT2:\n";
-print "\tfalse\n";
 for my $podcast (sort keys %db) {
-  print map {"\tid3v2 --TIT2 \"title\" \"$_\"\n"} (sort @{$db{$podcast}});
+  for my $episode (sort @{$db{$podcast}}) {
+    my $comment = $titles{$episode} ne "" ? "# " : "";
+
+    print "\t${comment}id3v2 --TIT2 \"$titles{$episode}\" \"$episode\"\n";
+  }
 }
 print "\n";
 
